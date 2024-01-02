b408774c1da4ad77fcd098895b98367394106022
John Galt <johngaltfirstrun@gmail.com>
2024-01-02 11:36:34 -0500

    vendor: Add preinstalled package list to stop auto cloning
    Only extra adds needed, gms now ships it's own lists for this.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- config/pixelstar.mk
+++ config/pixelstar.mk
@@ -2,3 +2,7 @@ ifeq ($(PIXELSTAR_BUILD_TYPE),Official)
 PRODUCT_PACKAGES += \
     Updater
 endif
+
+# Cloned app exemption
+PRODUCT_COPY_FILES += \
+    vendor/lineage/prebuilt/common/etc/sysconfig/preinstalled-packages-platform-everest-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/preinstalled-packages-platform-everest-product.xml
